aa5f90a883f6326eef79e5840ad554cd5133cfcf
Bahnasy2001 <hassanbahnasy872@gmail.com>
2025-06-17 22:56:15 +0300

    Author: Hassan Bahnasy Tool: Terraform Change: Create reusable module for secure S3 bucket to store ELB access logs with KMS encryption, versioning, lifecycle rules, and ELB logging policy support.

--- terraform/modules/s3_logs/main.tf
+++ terraform/modules/s3_logs/main.tf
@@ -86,6 +86,10 @@ resource "aws_s3_bucket_policy" "elb_logging_policy" {
   policy = data.aws_iam_policy_document.elb_logging.json
 }
 
+resource "aws_s3_bucket" "elb_logs_access" {
+  bucket = "${var.bucket_name}-access"
+}
+
 resource "aws_s3_bucket_logging" "elb_logs" {
   bucket = aws_s3_bucket.elb_logs.id
 
